0b2222c8779400733ad9ab07eb707089bd72f9c6
majapavlovic <maja.pavlovic2703@gmail.com>
2022-03-14 22:17:50 +0100

    kreirana klasa MagacinImpl koja implementira interfejs Magacin

---
+++ src/impl/MagacinImpl.java
@@ -0,0 +1,26 @@
+package impl;
+
+import domain.Artikal;
+import inferface.Magacin;
+
+public class MagacinImpl implements Magacin {
+
+	@Override
+	public void dodajArtikal(Artikal a) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal a) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Artikal nadjiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
