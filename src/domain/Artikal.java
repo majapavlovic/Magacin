d82e9c7826b806b1409fa69c121f09bc3bcba87c
majapavlovic <maja.pavlovic2703@gmail.com>
2022-03-14 20:03:20 +0100

    Generisane metode toString, hashCode i equals

--- src/domain/Artikal.java
+++ src/domain/Artikal.java
@@ -30,4 +30,19 @@ public class Artikal {
 	public void setSifra(int sifra) {
 		this.sifra = sifra;
 	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
 }
