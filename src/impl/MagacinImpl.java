a9a1f681378ca067ef95cbeab15ef07df16cd743
majapavlovic <maja.pavlovic2703@gmail.com>
2022-03-14 22:33:24 +0100

    Prilagodjene metode klase MagacinImpl

--- src/impl/MagacinImpl.java
+++ src/impl/MagacinImpl.java
@@ -1,25 +1,38 @@
 package impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import domain.Artikal;
 import inferface.Magacin;
 
 public class MagacinImpl implements Magacin {
 
+	List<Artikal> lista;
+
+	public MagacinImpl() {
+		lista = new ArrayList<>();
+	}
+
 	@Override
 	public void dodajArtikal(Artikal a) {
-		// TODO Auto-generated method stub
+		if (a != null) {
+			lista.add(a);
+		}
 
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal a) {
-		// TODO Auto-generated method stub
-
+		lista.remove(a);
 	}
 
 	@Override
 	public Artikal nadjiArtikal(int sifra) {
-		// TODO Auto-generated method stub
+		for (Artikal a : lista) {
+			if (a.getSifra() == sifra)
+				return a;
+		}
 		return null;
 	}
 
